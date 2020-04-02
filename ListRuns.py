ffc6bb1c57f618908c047d583e158695df58cf81
Guillermo Fidalgo <guillermo.fidalgo@upr.edu>
2020-04-03 00:21:41 +0200

    develping changes to RR api section

--- ListRuns.py
+++ ListRuns.py
@@ -42,14 +42,14 @@ def getruntype_eos(runtype="ZeroBias"):
 
 
 
-def getRR(first=272000,last=326000,tkr_IN=True,tkr_strip_ON=True,tkr_pix_ON=True, name= 'ULReReco2018',cl='Collisions'):
+def getRR(first=272000,last=326000,tkr_IN=True,tkr_strip_ON=True,tkr_pix_ON=True, name= 'UL',cl='Collisions'):
     runs = runregistry.get_runs(filter={
         'run_number': { 'and': [{'>': first}, {'<': last}]},
         'tracker_included': tkr_IN,
         'tracker-strip': tkr_strip_ON,
         'tracker-pixel': tkr_pix_ON,
-        'class': {  'like': '%cl%' },
-	#'name':  {  'like': '%name%'  },
+        'class': {  'like': '%{}%'.format(cl) },
+	'name':  {  'like': '%{}%'.format(name)  },
         })
         
     return runs
